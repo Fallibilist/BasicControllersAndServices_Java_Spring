760d3c37ffabb7191319cf1b861157bdd6f30332
unknown <Greg.Marshall.Hill@gmail.com>
2017-09-12 16:18:19 -0500

    Refactored project

---
+++ basiccontrollersandservices/src/main/java/com/cooksys/basiccontrollerandservicesassignment/BasicControllerAndServicesAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.cooksys.basiccontrollerandservicesassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicControllerAndServicesAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicControllerAndServicesAssignmentApplication.class, args);
+	}
+}
---
+++ basiccontrollersandservices/src/test/java/com/cooksys/basiccontrollerandservicesassignment/BasicControllerAndServicesAssignmentApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cooksys.basiccontrollerandservicesassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicControllerAndServicesAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
